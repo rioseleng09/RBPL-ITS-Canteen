174eac786ad85ecbe51a476a4865c4a5b9bf1b19
rioseleng09 <94964007+rioseleng09@users.noreply.github.com>
2023-05-28 16:02:09 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,35 +5,30 @@
     <link href="https://fonts.googleapis.com/css?family=Arial&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Manrope&display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
-    <link href="./css/main.css" rel="stylesheet" />
+    <link href="{{asset('login/css/main.css')}}" rel="stylesheet" />
     <title>Document</title>
 </head>
-
 <body>
-    <div class="v1_40">
-        <div class="v1_41">
-            <div class="v1_42">
-                <div class="name"></div>
-                <div class="v1_46"></div>
-            </div>
-            <div class="name"></div>
-            <div class="name"></div><span class="v1_56">9:41</span>
-        </div>
-        <div class="v1_57"><span class="v1_58">Don’t have an account ? </span><span class="v1_59">Sign Up</span></div>
-        <span class="v1_60">Hello again, you’ve been missed!</span><span class="v1_61">Hi, Wecome Back! 👋</span>
-        <div class="name"></div>
-        <div class="name"></div><span class="v1_64">Please Enter Your email</span>
-        <div class="v1_65">
-            <div class="v1_66"></div>
-            <div class="v1_67"></div>
-        </div>
-        <div class="name"></div>
-        <div class="name"></div>
-        <div class="name"></div>
-        <div class="v1_71"></div>
-        <div class="name"></div>
-        <div class="v1_75"></div>
+    <div class="v4_103">
+      <div class="v4_104">
+        <span class="v4_105">Don’t have an account ? </span
+        ><span class="v4_106">Sign Up</span>
+      </div>
+      <span class="v4_107">Hello again, you’ve been missed!</span
+      ><span class="v4_108">Hi, Wecome Back! 👋</span>
+      <div class="name"></div>
+      <div class="name"></div>
+      <span class="v4_111">Please Enter Your email</span>
+      <div class="v4_112">
+        <div class="v4_113"></div>
+        <div class="v4_114"></div>
+      </div>
+      <div class="name"></div>
+      <div class="name"></div>
+      <div class="name"></div>
+      <div class="v4_118"></div>
+      <div class="name"></div>
+      <div class="v4_120"></div>
     </div>
-</body>
-
+  </body>
 </html>
